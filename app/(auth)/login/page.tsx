e386c029102896e51469fb4b7b8553b9aa0a33f1
Angel <angeloktober00@gmail.com>
2024-10-16 16:53:15 +0700

    Add login card

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import Card from "@/components/ui/Card"
+
+const LoginPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <Card />
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ components/ui/Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className='rounded-xl bg-white p-8 flex flex-col justify-start items-center gap-8 shadow-lg w-[400px] h-[500px] max-w-[600px]'>
+            <div className='flex justify-between items-center w-full mb-4'></div>
+            <p className='text-gray-500'>
+                Test
+            </p>
+        </div>
+    )
+}
+
+export default Card
