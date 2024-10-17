14a0cec78056a0940ce73e1336f2ca6cb37668ab
Angel <angeloktober00@gmail.com>
2024-10-17 09:40:35 +0700

    Edit Card Styling

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ width = 'w-[400px]', height = 'h-[500px]'}) => {
     return (
-        <div className='rounded-xl bg-white p-8 flex flex-col justify-start items-center gap-8 shadow-lg w-[400px] h-[500px] max-w-[600px]'>
-            <div className='flex justify-between items-center w-full mb-4'></div>
-            <p className='text-gray-500'>
-                Test
-            </p>
+        <div className={`rounded-xl bg-white p-8 flex flex-col justify-start items-center gap-8 shadow-lg ${width} ${height} max-w-[600px]`}>
+            {/* Card Content Here */}
         </div>
     )
 }
