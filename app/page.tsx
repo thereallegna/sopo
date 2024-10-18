db83b636c6edc3799a4f706950618fb400b7cdf8
Raihan <raihanadinugroho9g26@gmail.com>
2024-10-18 10:48:27 +0700

    feat: add Checkbox Component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { Checkbox } from '@components/ui/Checkbox';
 
 const Home = () => (
   <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -14,6 +15,8 @@ const Home = () => (
         height={38}
         priority
       />
+      <Checkbox />
+      <Checkbox preChecked />
     </main>
   </div>
 );
