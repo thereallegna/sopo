f07818c84a73446003a454f5d5f42d31a0b63b89
GymnastiarAlmaGhifari <e41211478@student.polije.ac.id>
2024-10-18 14:47:56 +0700

    optimize button

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -4,20 +4,20 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@libs/classNames';
+import IconComponent from '@components/ui/Icon';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm  shadow-Shadow/s px-3 py-2 ring-offset-white focus:ring-offset-0 focus:ring-Blue-200 focus:ring-[3px] w-full',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm shadow-Shadow/s px-3 py-2 ring-offset-white focus:ring-offset-0 focus:ring-Blue-200 focus:ring-[3px] w-full',
   {
     variants: {
       variant: {
-        primary:
-          'bg-Blue-500 text-Neutral-White hover:bg-neutral-900/90 hover:bg-Blue-700 ',
+        primary: 'bg-Blue-500 text-white hover:bg-Blue-700',
         outlined:
-          'border border-Blue-700 text-Blue-600 bg-white hover:border-Blue-700 hover:text-Blue-700 ',
+          'border border-Blue-700 text-Blue-600 bg-white hover:text-Blue-700',
       },
       size: {
         small: 'font-semibold text-base',
-        normal: 'font-semibold text-lg ',
+        normal: 'font-semibold text-lg',
         big: 'font-bold text-xl',
         icon: 'h-10 w-10',
       },
@@ -34,6 +34,8 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
   icon?: React.ElementType;
+  iconClassName?: string;
+  iconSize?: 'small' | 'medium' | 'large' | null;
 }
 
 // Button Component
@@ -43,6 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       className,
       variant,
       size,
+      iconSize,
       asChild = false,
       icon: Icon,
       children,
@@ -51,19 +54,26 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) => {
     const Comp = asChild ? Slot : 'button';
+
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))} //
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
-        {Icon && <Icon className="w-5 h-5" />}
+        {Icon && (
+          <IconComponent
+            icon={Icon}
+            color={variant === 'primary' ? 'White' : 'secondary'}
+            size={iconSize || 'medium'}
+          />
+        )}
         {children}
       </Comp>
     );
   }
 );
 
-Button.displayName = 'Button'; // Nama untuk debugging
+Button.displayName = 'Button'; // Name for debugging
 
 export { Button, buttonVariants };
