e7ad1f4d488e7597a9e8b43d69f5def1ce1b7cff
GymnastiarAlmaGhifari <e41211478@student.polije.ac.id>
2024-10-23 15:57:42 +0700

    add sidebar header ui

--- components/module/Sidebar/SidebarFooter.tsx
+++ components/module/Sidebar/SidebarFooter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SidebarFooter: React.FC = () => (
-  <div className="sticky bottom-0-0 bg-red-500 h-12 w-full">
+  <div className="sticky bottom-0  h-12 w-full">
     <p>SidebarFooter </p>
   </div>
 );
--- components/module/Sidebar/index.tsx
+++ components/module/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import SidebarContent from './SidebarContent';
 import SidebarFooter from './SidebarFooter';
 
 const Sidebar: React.FC = () => (
-  <div className="w-[240px] sidebar-height p-3 bg-emerald-500 text-white flex flex-col ">
+  <div className="w-[240px] sidebar-height p-3  flex flex-col ">
     <SidebarHeader />
     <SidebarContent />
     <SidebarFooter />
