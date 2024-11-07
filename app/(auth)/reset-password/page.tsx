633154677e6545b6a3592bc19c226269c0f2fa52
Angel <angeloktober00@gmail.com>
2024-11-07 11:55:44 +0700

    adding combobox component

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -14,12 +14,46 @@ import {
 import Image from 'next/image';
 import { IconArrowLeft } from '@tabler/icons-react';
 import IconComponent from '@components/ui/Icon';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import { resetPasswordConstant } from '@constants/resetPasswordConstant';
 
 const ResetPasswordPage = () => {
   const router = useRouter();
-  const [password, setPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  const searchParams = useSearchParams();
+  const resetToken = searchParams.get('token');
+
+  const handleResetPassword = async () => {
+    if (newPassword !== confirmPassword) {
+      setError('Password do not match.');
+      return;
+    }
+    setError('');
+    try {
+      const response = await fetch('/api/reset-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ newPassword, token: resetToken }),
+      });
+
+      if (response.ok) {
+        setSuccess(true);
+        router.push('/login');
+      } else {
+        const result = await response.json();
+        setError(result.error || 'Failed to reset password. Please try again.');
+      }
+    } catch (err) {
+      setError('An error occurred. Please try again');
+    }
+  };
 
   return (
     <div className="flex justify-center items-center">
@@ -39,28 +73,45 @@ const ResetPasswordPage = () => {
           <IconComponent
             icon={IconArrowLeft}
             className="mr-2"
-            onClick={() => router.push('/forgot-password')}
+            onClick={() => router.push('/login')}
           />
           Reset Password
         </CardTitle>
         <CardDescription className="text-[11px] font-normal mt-1">
           Enter a new password below to change your password
         </CardDescription>
-        <CardContent className="mt-5">
-          <InputField
-            placeholder="Input your new password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <InputField
-            className="mt-[10px]"
-            placeholder="Confirm your new password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
+        <CardContent className="mt-[10px]">
+          {resetPasswordConstant.inputFields.map((inputField, index) => (
+            <InputField
+              key={inputField.label}
+              label={inputField.label}
+              placeholder={inputField.placeholder}
+              start_icon={inputField.start_icon}
+              type={inputField.type}
+              value={index === 0 ? newPassword : confirmPassword}
+              onChange={(e) => {
+                if (inputField.label === 'New Password') {
+                  setNewPassword(e.target.value);
+                } else {
+                  setConfirmPassword(e.target.value);
+                }
+              }}
+              className={index === 1 ? 'mt-[10px]' : ''}
+            />
+          ))}
+          {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+          {success && (
+            <p className="text-green-500 text-sm mt-2">
+              Password reset successfully. Redirecting...
+            </p>
+          )}
         </CardContent>
         <CardFooter className="mt-[10px]">
-          <Button type="submit" className="w-full h-6">
+          <Button
+            type="submit"
+            className="w-full h-6"
+            onClick={handleResetPassword}
+          >
             Reset Password
           </Button>
         </CardFooter>
---
+++ constants/forgotPasswordConstant.ts
@@ -0,0 +1,11 @@
+import { IconMail } from '@tabler/icons-react';
+
+export const forgotPasswordConstant = {
+  inputField: [
+    {
+      label: 'Email',
+      placeholder: 'Input your email',
+      start_icon: { icon: IconMail },
+    },
+  ],
+};
