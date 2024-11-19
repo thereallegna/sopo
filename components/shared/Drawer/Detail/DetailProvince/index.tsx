a88f2a379dbe7ad73e57a256a26ff1b8b49cf7f1
GymnastiarAlmaGhifari <e41211478@student.polije.ac.id>
2024-11-19 16:26:03 +0700

    add: province timline

--- components/shared/Drawer/Detail/DetailProvince/index.tsx
+++ components/shared/Drawer/Detail/DetailProvince/index.tsx
@@ -7,18 +7,24 @@ import {
   DrawerBody,
   DrawerContent,
   DrawerEndHeader,
+  DrawerFooter,
   DrawerHeader,
 } from '@components/ui/Drawer';
 import { Card, CardContent } from '@components/ui/Card';
 import InputField from '@components/shared/InputField';
 import { useDrawerStore } from '@stores/useDrawerStore';
-import { IconPencil } from '@tabler/icons-react';
+import { IconHistory, IconPencil } from '@tabler/icons-react';
 import { useForm } from 'react-hook-form';
 import { useDetailForm } from '@hooks/useFormChanges';
 import Combobox from '@components/ui/Combobox';
 
 const DetailProvince = () => {
-  const { isOpenDetail, closeDetailDrawer, openEditDrawer } = useDrawerStore();
+  const {
+    isOpenDetail,
+    closeDetailDrawer,
+    openEditDrawer,
+    openHistoryLogDrawer,
+  } = useDrawerStore();
   const detail_data = useDrawerStore((state) => state.detail_data) as IProvince;
 
   const { register, setValue } = useForm<IProvince>();
@@ -76,6 +82,21 @@ const DetailProvince = () => {
               />
             </CardContent>
           </Card>
+          <DrawerFooter>
+            <Button
+              variant="backDrawer"
+              className="w-7"
+              size="icon"
+              icon={{ size: 'large', icon: IconHistory, color: 'dark' }}
+              type="submit"
+              onClick={() =>
+                openHistoryLogDrawer({
+                  code: detail_data?.province_code,
+                  category: 'Province',
+                })
+              }
+            />
+          </DrawerFooter>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
--- components/ui/Drawer/index.tsx
+++ components/ui/Drawer/index.tsx
@@ -165,7 +165,7 @@ const DrawerFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn(' flex flex-col gap-2 p-5', className)} {...props} />
+  <div className={cn(' flex flex-col gap-2 ', className)} {...props} />
 );
 DrawerFooter.displayName = 'DrawerFooter';
 
